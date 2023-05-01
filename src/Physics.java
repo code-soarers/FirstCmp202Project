44445accd644ecb8dc477324d94c1530853a52e4
vicksin6 <victorAmadi@bingham.edu.ng>
2023-05-01 16:16:57 +0000

    fir st fils added

---
+++ src/Physics.java
@@ -0,0 +1,21 @@
+public class Physics {
+
+    private float Gravity = 9.8f;
+
+    public float getGravity() {
+        return Gravity;
+    }
+
+    public void setGravity(float gravity) {
+        Gravity = gravity;
+    }
+
+    public static void main(String[] args) {
+
+
+
+
+
+
+    }
+}
