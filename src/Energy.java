46bae99846cd6370b1244f0e55e6c575f0344423
vicksin6 <victorAmadi@bingham.edu.ng>
2023-05-03 09:34:28 +0000

    i added a kinetic energy method

--- src/Energy.java
+++ src/Energy.java
@@ -1,6 +1,10 @@
-public class Energy extends Physics{
+public class Energy extends Physics {
 
-    float potentialEnergy(float mass, float height){
+    float potentialEnergy(float mass, float height) {
         return mass * getGravity() * height;
+    }
+
+    float kineticEnergy(float mass, float velocity){
+        return (1/2) * mass * (velocity * velocity);
     }
 }
