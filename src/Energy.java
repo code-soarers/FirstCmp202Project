2f9bd8f38f59cfbfb26d18686e8262d7a9518104
treasure evbooma <treasure.evbooma@binghamuni.edu.ng>
2023-05-04 15:49:54 +0100

    I added the nuclear energy method

--- src/Energy.java
+++ src/Energy.java
@@ -11,4 +11,8 @@ public class Energy extends Physics {
     float heatEnergy(float mass, float initialTemperature, float finalTemperature){
         return mass * 4.2f * (finalTemperature - initialTemperature);
     }
+    float nuclearEnergy(float mass){
+        return mass * 3.0*(Math.pow(10,8))*3.0*(Math.pow(10,8));
+    }
+
 }
